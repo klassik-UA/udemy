e9bfcfb193a3cc43f8eaa5c15f6eefcc0f421a9e
Topoliuk Illia <91085814+klassik-UA@users.noreply.github.com>
2022-01-04 19:24:03 +0200

    Add files via upload

---
+++ lesson 1.js
@@ -0,0 +1,16 @@
+'use strict'
+const numberOfFilms = 10;
+const personalMovieDB = {
+    count: 10,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const movies = {
+    'Pila': '9',
+    'NePila': '10',
+}
+console.log(numberOfFilms);
+console.log(personalMovieDB);
+console.log(movies);
